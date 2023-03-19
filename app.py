2ae293e02c79036ca6b33ac6a08832b7677bb916
Astraxx04 <gagan200254@gmail.com>
2023-03-19 21:20:54 +0530

    Data is taken from the db both venue and show

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import *
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'super secret key'
@@ -29,24 +30,33 @@ def load_user(user_id):
 
 #Models--------------------------------
 
-class Admins(db.Model):
-    admin_id = db.Column(db.Integer(), primary_key = True)
-    admin_name = db.Column(db.String(30), nullable = False)
-    password = db.Column(db.String(20), nullable = False)
+# class Admins(db.Model, UserMixin):
+#     admin_id = db.Column(db.Integer(), primary_key = True)
+#     admin_name = db.Column(db.String(30), nullable = False)
+#     password = db.Column(db.String(20), nullable = False)
 
-    def __repr__(self):
-        return "<Admin %r>" % self.admin_id
+#     def __repr__(self):
+#         return "<Admin %r>" % self.admin_id
 
 class Users(db.Model, UserMixin):
     user_id = db.Column(db.Integer(), primary_key = True)
     password = db.Column(db.String(20), nullable = False)
     usr_name = db.Column(db.String(30), nullable = False)
+    userType = db.Column(db.String(20), default = 'user')
 
     def get_id(self):
            return (self.user_id)
 
     def __repr__(self):
         return "<User %r>" % self.user_id
+
+    def isAdmin(self): 
+        if self.userType == 'admin':
+            return True
+        else:
+            return False
+
+
 
 class Venues(db.Model):
     venue_id = db.Column(db.Integer(), primary_key = True)
@@ -130,7 +140,6 @@ class NewTicketBookingForm(FlaskForm):
 
 
 
-
 #Routes--------------------------------------------------------------
 
 
@@ -145,7 +154,13 @@ def adminlogin():
     form = AdminLoginForm()
 
     if form.validate_on_submit():
-        return redirect(url_for('registeration'))
+        user = Users.query.filter_by(usr_name=form.adminname.data).first()
+        if user and Users.isAdmin(user):
+            if user.password == form.password.data:
+                login_user(user)
+                return redirect(url_for('admindashboard'))
+            else:
+                return '<h1>Invalid Password</h1>'
 
 
     return render_template('admin_login.html', title='Admin Login', form=form)
@@ -153,7 +168,7 @@ def adminlogin():
 
 
 @app.route('/userlogin', methods =["GET", "POST"])
-def userlogin():
+def login():
     form = UserLoginForm()
 
     if form.validate_on_submit():
@@ -184,18 +199,32 @@ def user_registeration():
 
 
 @app.route('/userdashboard', methods =["GET", "POST"])
+@login_required
 def userdashboard():
     return render_template('user_dashboard.html', title='User Dashboard')
 
 
 
 @app.route('/admindashboard', methods =["GET", "POST"])
+@login_required
 def admindashboard():
-    return render_template('admin_dashboard.html', title='Admin Dashboard')
+    venues = Venues.query.all()
+    venu=[]
+    for ven in venues:
+        shows = Shows.query.filter(ven.venue_id==Shows.svenue_id).all()
+        show=[]
+        for sho in shows:
+            print(sho.show_name)
+            show.append({"name": sho.show_name, "time": sho.show_time})
+        venu.append({"name": ven.venue_name, "cards": show})
+    print(venu)
+
+    return render_template('admin_dashboard.html', title='Admin Dashboard', data=venu)
 
 
 
 @app.route('/ticketbooking', methods =["GET", "POST"])
+@login_required
 def ticketbooking():
     form = NewTicketBookingForm()
 
--- static/js/check3.js
+++ static/js/check3.js
@@ -1,12 +1,17 @@
 var btn = 0;
 const venuesContainer = document.getElementById("venues-container");
 
-const venueData = [
-	{ name: "Venue 1",  cards: [ { name: "Show 1", time: 30 }, { name: "Show 2", time: 25 }, { name: "Show 3", time: 40 } ] },
-	{ name: "Venue 2",  cards: [ { name: "Show 4", time: 30 }, { name: "Show 5", time: 25 }, { name: "Show 6", time: 40 } ] },
-	{ name: "Venue 3",  cards: [ { name: "Show 7", time: 30 }, { name: "Show 8", time: 25 }, { name: "Show 9", time: 40 }, { name: "Show 10", time: 25 } ] }
-];
+// const venueData = [
+// 	{ name: "Venue 1",  cards: [ { name: "Show 1", time: 30 }, { name: "Show 2", time: 25 }, { name: "Show 3", time: 40 } ] },
+// 	{ name: "Venue 2",  cards: [ { name: "Show 4", time: 30 }, { name: "Show 5", time: 25 }, { name: "Show 6", time: 40 } ] },
+// 	{ name: "Venue 3",  cards: [ { name: "Show 7", time: 30 }, { name: "Show 8", time: 25 }, { name: "Show 9", time: 40 }, { name: "Show 10", time: 25 } ] }
+// ];
+console.log(data);
+var venueData = data;
 const numVenueToPrint = venueData.length;
+
+// --- Global variable keeping track of number of venues---
+var venues = venueData.length;
 
 for (let i = 0; i < numVenueToPrint; i++) {
 	// Create a new card element
@@ -66,25 +71,21 @@ function createShows(x) {
     btn = btn + 1;
     const card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<button class="showadd_button" onclick="addShow(this)" id="plusBtnshow${btn}" href=""{{ url_for('newvenue') ><img class="add-show-img" id="add-show-img" src="static/images/plus_icon.png" alt="Add a new Show"></button>`;
+    card.innerHTML = `<button class="showadd_button" onclick="addShow(this)" id="plusBtnshow${btn}" href=""{{ url_for('newshow') ><img class="add-show-img" id="add-show-img" src="static/images/plus_icon.png" alt="Add a new Show"></button>`;
     // card.setAttribute('id',`plusBtnshow${btn}`);
     // Add the card element to the card container
     showsContainer.appendChild(card);
 }
 
+// --- Function responsible for changing the page ---
+function addShow(element) {
+    let cookie = element.id;
+    window.location.href = 'newshow';
+    console.log(element.id);
+    sessionStorage.setItem('show_id', cookie);
+}
 
 
-
-
-
-//Adding new venues
-const myDiv = document.getElementById('venueadd_button');
-  // Add a click event listener to the div
-  myDiv.addEventListener('click', function() {
-    
-});
-myDiv.classList.remove('vvcard');
-myDiv.style = '';
 
 
 
@@ -93,23 +94,7 @@ const addChildBtn = document.querySelector('#venueadd_button');
 const btnCard = document.querySelector('#plusBtnvenue');
 
 addChildBtn.addEventListener('click', () => {
-    const vcard = document.createElement("div");
-	vcard.classList.add("vcard");
-
-	// Add the card data to the element
-	vcard.innerHTML = `
-		<h1>Venue x</h1>
-	`;
-    const showsContainer = document.getElementById("shows-container"+(numVenueToPrint));
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<button class="showadd_button" onclick="addShow(this)" id="plusBtnshow${btn} href=""{{ url_for('newvenue') " ><img class="add-show-img" id="add-show-img" src="static/images/plus_icon.png" alt="Add a new Show"></button>`;
-    // card.setAttribute('id',`plusBtnshow${btn}`);
-    // Add the card element to the card container
-    console.log(card);
-    showsContainer.appendChild(card);
-
-
-
-    btnCard.parentNode.insertBefore(vcard, btnCard);
+    window.location.href = 'newvenue';
+    venues+=1;
+    sessionStorage.setItem('venue_no', venues + 1);
 });
